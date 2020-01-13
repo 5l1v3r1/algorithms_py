d6a4f3d9c4b3e56a83c6b56d716c20a1faf2db39
Bot3939 <39862138+Bot3939@users.noreply.github.com>
2020-01-13 11:38:12 -0300

    implemented the bogo sort algorithm

---
+++ sort/bogosort.py
@@ -0,0 +1,18 @@
+# implementation of the bogosort algorithm
+# extremely inefficient
+from random import shuffle
+
+def isSorted(l):
+    previous = l[0]
+    for i in range(1, len(l)):
+        if l[i] < previous:
+            return False
+        previous = l[i]
+    return True
+
+def sort(l):
+    while not isSorted(l):
+        shuffle(l)
+    return l
+
+print(sort([2,3,1,8,4,6,5,9]))
