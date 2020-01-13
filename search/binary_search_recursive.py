fe9a4d39f6ab64d56b7651b5387ffc3b16c087e5
Bot3939 <39862138+Bot3939@users.noreply.github.com>
2020-01-12 22:03:05 -0300

    removed unnecessary argument in recursive binary search

--- search/binary_search_recursive.py
+++ search/binary_search_recursive.py
@@ -1,16 +1,16 @@
 # recursive implementation of the binary search algorithm
 # this algorithm only works with sorted arrays
-def find(x, l, left, right, mid):
+def find(x, l, left, right):
     mid = left + int((right - left) / 2)
     if left > right:
         return -1
     elif l[mid] == x:
         return mid
     elif x > l[mid]:
-        return find(x, l, mid+1, right, mid)
+        return find(x, l, mid+1, right)
     else:
-        return find(x, l, left, mid-1, mid)
+        return find(x, l, left, mid-1)
 
 
-li = list(range(0, 100))
-print(find(1, li, 0, len(li)-1, 0))
+li = list(range(37, 100))
+print(find(38, li, 0, len(li)-1))
