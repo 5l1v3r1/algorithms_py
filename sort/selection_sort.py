a9c8a639f0bd4e505baa01b1eaf74ec304f2508a
Bot3939 <39862138+Bot3939@users.noreply.github.com>
2020-01-15 17:48:20 -0300

    better implementation of selection sort

--- sort/selection_sort.py
+++ sort/selection_sort.py
@@ -2,12 +2,11 @@
 
 def sort(l):
     for i in range(len(l)):
-        min_num = l[i]
+        min_ind = i
         for j in range(i+1, len(l)):
-            if l[j] < min_num:
-                min_num, l[j] = l[j], min_num
-        l[i] = min_num
-
+            if l[j] < l[min_ind]:
+                min_ind = j
+        l[i], l[min_ind] = l[min_ind], l[i]
     return l
 
 print(sort([3,2,4,1]))
